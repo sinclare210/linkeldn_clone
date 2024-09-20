7c9b9cf317cd60f3f34568aca7c2bf78c7b26df2
Sinclair <sinclairolajuwon@gmail.com>
2024-09-19 19:24:13 -0500

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login'
+import Home from './components/Home'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+
           <Route path='/' element={<Login />} />
+
+
+          <Route 
+            path='/home' 
+            element={
+              <>
+                <Header /> 
+                <Home />
+              </>
+            } 
+          />
         </Routes>
       </Router>
     </>
