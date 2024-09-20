fb08e29bb451d805d4bc5924f873edb39b865472
Sinclair <sinclairolajuwon@gmail.com>
2024-09-20 09:09:05 -0500

    done with header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,27 @@ const Header = (props) => {
                     <span>Notifications</span>
                 </a>
             </NavList>  
-              
+              <User>
+                <a>
+                  <img src="src/images/user.svg" alt="" />
+                  <span>Me</span>
+                  <img src="src/images/down-icon.svg" alt="" />
+                </a>
+                <SignOut>
+                  <a>
+                    Sign Out
+                  </a>
+                </SignOut>
+              </User>
+             <Work>
+              <a>
+                <img src="src/images/nav-work.svg" alt="" />
+                <span>
+                  Work
+                  <img src="src/images/down-icon.svg" alt="" />
+                </span>
+              </a>
+            </Work>
         </NavListWrap>
             </Nav>
         </Content>
@@ -199,6 +219,53 @@ const NavList = styled.li`
     }
   }
 `;
+
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  display: none;
+  text-align: center;
+
+`;
+
+const User = styled(NavList)`
+  a > svg{
+    width: 24px;
+    border-radius: 50%;
+  }
+
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    border-radius: 50%;
+  }
+
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0,0,0,0.08);
+`;
+
+
 
 
 export default Header
