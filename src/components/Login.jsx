80afce8a20cac2f81832005e98d5e555aafbb501
Sinclair <sinclairolajuwon@gmail.com>
2024-09-23 17:40:24 -0500

    done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ const Login = (props) => {
     <Section>
         <Hero>
             <h1>Welcome to your professional community</h1>
-            <img src="./images/login-hero.svg" alt="" />
+            <img src="../images/login-hero.svg" alt="" />
         </Hero>
         <Form>
             <Google onClick={() => props.signIn()}>
