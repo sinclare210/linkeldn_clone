46fac228b4c2bc36e0e199130f824e4c0b5d2440
Sinclair <sinclairolajuwon@gmail.com>
2024-09-29 13:29:37 -0500

    some changes

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -6,10 +6,7 @@ import Header from './Header'
 const Job = (props) => {
   return (
     <>
-     {
-              props.user &&
-        <Navigate to="/Job" replace={true} />
-      }
+   
     
     <div>Job</div>
      <Header/>
